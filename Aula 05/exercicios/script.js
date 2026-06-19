1e9af19914c71260e86d7c9461e934ef2f4357ab
nathan270109 <natiel228@gmail.com>
2026-06-19 14:15:02 -0300

    atualizar-19.06

---
+++ Aula 05/exercicios/script.js
@@ -0,0 +1,112 @@
+// DESAFIO 1 ========================================
+
+// for(let i = 1; i <= 10; i++){
+//     console.log(i)
+// }
+
+// DESAFIO 2 =========================================
+
+// for(let i = 1; i <= 20; i++){
+//     if(i % 2 == 0){
+//         console.log(i)
+//     }
+// }console.log("LANÇAMENTO")
+
+//  ============================================
+
+// for(let i = 5; i >= 0; i--){
+//     console.log(i)
+// }
+
+//  ============================================
+
+// let number = 9
+
+// for(let i = 1; i <= 10; i++){
+//     console.log(number + " x " + i + " = " + (number * i))
+// }
+
+// WHILE ==========================================
+
+// let bateria = 100;
+
+// while(bateria > 0){
+//     console.log("Bateria:" + bateria + "%")
+//     bateria -= 20;
+// }
+
+// ==================================================
+
+// let senha = "";
+
+// while(senha !== "123"){
+//     senha = prompt("Digite a senha")
+// }
+
+// alert("acesso liberado")
+
+// 4 DESAFIO =======================================
+
+// let energia = 100
+
+// while(energia > 0){
+//     energia -= 10
+//     console.log(energia)
+// }
+
+// DO WHILE ===================================================
+
+// let credito = 0
+
+// do{
+//     console.log("tentando jogar...")
+// }while(credito > 0)
+
+// DESAFIO 5 ===============================================================
+
+// let opcao
+
+// do{
+//     opcao = Number(prompt("1 - Jogar - 2 - Ranking - 3 - Sair"))
+//     switch(opcao){
+//         case 1: alert("Jogando...")
+//         break
+//         case 2: alert("Seu Ranking é Top 10")
+//         break
+//         case 3: alert("Saindo")
+//         break
+//         default: alert("Inválido")
+//         break
+//     }
+// }while(opcao !== 3)
+
+// VETORES ================================================================
+
+// let frutas = [
+//     "Maçã",
+//     "Banana",
+//     "Uva"
+// ]
+
+// console.log(frutas[1])
+
+//==============================================================================
+
+// let frutas = [];
+
+// frutas.push("Maçã");
+// frutas.push("Uva");
+
+// ["Maçã", "Uva"]
+
+// ===========================================================================
+
+// let frutas = [
+//     "Maçã",
+//     "Uva",
+//     "Laranja",
+//     "Banana"
+// ]
+// console.log(frutas.length)
+
+// DESAFIO 6 =======================================================
